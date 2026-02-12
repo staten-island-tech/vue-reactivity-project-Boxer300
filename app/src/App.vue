05ecd79eb7a76a9777bb774929890cbef1d4cfff
Boxer300 <romano2789@nycstudents.net>
2026-02-12 12:34:48 -0500

    zdsgg

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,169 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+
+  </div>
 </template>
 
-<style scoped></style>
- babaabasbakbas 
- Lets see if he checks
+<script setup>
+const items =[
+    {
+        name: "Sponge bob cleats",
+        price: 119.99,
+        inStock: true,
+        brand: "Sponge bob",
+        img: "https://phenomelitebrand.com/cdn/shop/files/SpongebobProductShot4_535x.png?v=1740765484",
+        alt: "Cleats",
+    },
+        {
+        name: "Ukrainian Jersey",
+        price: 34.99,
+        inStock: true,
+        brand: "Adidas",
+        img: "img/116401-removebg-preview.png",
+        alt: "Jersey",
+    },    
+           {
+        name: "2022 World Cup Soccer Ball",
+        price: 39.99,
+        inStock: true,
+        brand: "Adidas",
+        img: "img/hd-qatar-world-cup-2022-ball-png-7040816948791863showibkng-removebg-preview.png",
+        alt: "Soccer ball",
+    },        {
+        name: "Cleats",
+        price: 89.99,
+        inStock: true,
+        brand: "Nike",
+        img: "img/imgbin-nike-mercurial-vapor-football-boot-cleat-nike-FeKUXXVBpwvUrZ9cpgQUxBzrz-removebg-preview.png",
+        alt: "Nike cleats",
+    },        {
+        name: "Soccer ball",
+        price: 19.99,
+        inStock: true,
+        brand: "Generic",
+        img: "img/1000_F_190984838_0fvehfxAtlLrwREWAYkS0pmZknmHlA7c-removebg-preview.png",
+        alt: "Soccer ball",
+    },       
+     {
+        name: "Barcelona Jersey",
+        price: 34.99,
+        inStock: true,
+        brand: "Nike",
+        img: "img/5bc1a53012e3b-removebg-preview.png",
+        alt: "Jersey",
+    },     {
+        name: "Goalkeeping gloves",
+        price: 34.99,
+        inStock: true,
+        brand: "Adidas",
+        img: "img/images-removebg-preview.png",
+        alt: "gloves",
+    },        {
+        name: "U.S. Jersey",
+        price: 34.99,
+        inStock: true,
+        brand: "Nike",
+        img: "img/third-jersey-united-states-men-s-national-soccer-team-shirt-kit-shirt-removebg-preview.png",
+        alt: "Jersey",
+    },       {
+    name: "Liverpool Jersey",
+    price: 34.99,
+    inStock: true,
+    brand: "New Balance",
+    img: "img/t-shirt-liverpool-f-c-jersey-kit-t-shirt-removebg-preview.png",
+    alt: "Jersey",
+  },
+  {
+    name: "Soccer ball",
+    price: 29.99,
+    inStock: true,
+    brand: "Nike",
+    img: "img/soccer-ball-hk1099-premier-league-winter-ball-2018-19-11563421364hwhip7iy4d-removebg-preview.png",
+    alt: "Soccer ball",
+  },
+  {
+    name: "Barcelona shorts",
+    price: 14.99,
+    inStock: true,
+    brand: "Nike",
+    img: "img/avuw9xoqg-removebg-preview.png",
+    alt: "shorts",
+  },
+  {
+    name: "Black shorts",
+    price: 9.99,
+    inStock: true,
+    brand: "Generic",
+    img: "img/ITAM_Essential_AthleticShorts_Black-removebg-preview.png",
+    alt: "shorts",
+  },
+  {
+    name: "White shorts",
+    price: 9.99,
+    inStock: true,
+    brand: "Generic",
+    img: "img/mockup-of-a-white-sports-pant-free-png-removebg-preview.png",
+    alt: "shorts",
+  },    
+  {
+  name: "Golden cleats",
+  price: 199.99,
+  inStock: true,
+  brand: "Nike",
+  img: "https://png.pngtree.com/png-vector/20250112/ourmid/pngtree-golden-football-cleat-clipart-illustration-png-image_15160443.png",
+  alt: "Cleats",
+},
+{
+  name: "MLS ball",
+  price: 9.99,
+  inStock: true,
+  brand: "Adidas",
+  img: "img/seo-gifts-for-soccer-fans-body-image-1_221-578642-removebg-preview.png",
+  alt: "Soccer ball",
+},
+{
+  name: "Orange and white cleats",
+  price: 99.99,
+  inStock: true,
+  brand: "Generic",
+  img: "img/pngtree-durable-soccer-cleats-for-outdoor-games-png-image_13451541-removebg-preview.png",
+  alt: "Cleats",
+},
+{
+  name: "Blue gloves",
+  price: 9.99,
+  inStock: true,
+  brand: "Nike",
+  img: "img/275-2754018_goal-keeping-glove-png-photo-background-nike-goalkeeper-removebg-preview.png",
+  alt: "Gloves",
+},
+{
+  name: "Red shorts",
+  price: 15.99,
+  inStock: true,
+  brand: "Adidas",
+  img: "img/461686s-removebg-preview.png",
+  alt: "Shorts",
+},
+{
+  name: "Red gloves",
+  price: 9.99,
+  inStock: true,
+  brand: "Adidas",
+  img: "img/pngtree-red-goalkeeper-gloves-png-image_16215402-removebg-preview.png",
+  alt: "Gloves",
+},
+{
+  name: "Blue and yellow gloves",
+  price: 9.99,
+  inStock: true,
+  brand: "Kipsta",
+  img: "img/620-6206722_goal-keeping-glove-png-transparent-image-kipsta-goalkeeper-removebg-preview.png",
+  alt: "Gloves",
+},   
+];
+</script>
+
+<style lang="scss" scoped>
+
+</style>
